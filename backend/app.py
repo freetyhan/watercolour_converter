3d2d6667756b4d0bc5f29e4ef9799421229d296a
Adriana Castro <adriana.e.castro@outlook.com>
2021-03-29 23:05:13 -0700

    have a clickable box - need to get image overlays

--- backend/app.py
+++ backend/app.py
@@ -9,22 +9,21 @@ app.config.update(
     DROPZONE_MAX_FILE_SIZE = 1024,
     DROPZONE_ALLOWED_FILE_CUSTOM = True,
     DROPZONE_ALLOWED_FILE_TYPE = 'image/*, video/*',
-    #DROPZONE_REDIRECT_VIEW = {{ dropzone.config(redirect_url=url_for('endpoint', foo=bar)) }},
+    DROPZONE_DEFAULT_MESSAGE = "",
     DROPZONE_INVALID_FILE_TYPE = "Please upload a photo or video",
-    DROPZONE_UPLOAD_MULTIPLE = False,
     DROPZONE_TIMEOUT = 5*60*1000)
 
-
+dropzone = Dropzone(app)
 
 @app.route('/',methods=['POST','GET'])
 def upload():
     if request.method == 'POST':
         f = request.files.get('file')
         f.save(os.path.join(app.config['UPLOADED_PATH'],f.filename))
-    return render_template('index.html')
+    return render_template('WaterColourConverter.html')
 
-#@app.route('/results/')
-#def result():
+# @app.route('/results/')
+# def result():
 #     return "result"
 
 if __name__ == '__main__':
