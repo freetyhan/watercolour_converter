987c358e9f279099739c23294096ef56b31f8d43
Adriana Castro <adriana.e.castro@outlook.com>
2021-04-10 12:32:58 -0700

    changes to app for han

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import os
+from flask import send_from_directory
 from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,6 +7,8 @@ app = Flask(__name__)
 
 app.config.update(
     UPLOADED_PATH= os.path.join(basedir,'uploads'),
+    RESULT_PATH= os.path.join(basedir,'results'),
+    IMAGES_PATH= os.path.join(basedir,'images'),
     DROPZONE_MAX_FILE_SIZE = 1024,
     DROPZONE_ALLOWED_FILE_CUSTOM = True,
     DROPZONE_ALLOWED_FILE_TYPE = 'image/*, video/*',
@@ -28,6 +31,14 @@ def upload():
 @app.route('/results/')
 def resultPage():
      return render_template('index.html')
+
+@app.route('/results/<filename>', methods=['GET', 'POST'])
+def download(filename):
+    return send_from_directory(directory=app.config['RESULT_PATH'], filename=filename)
+
+@app.route('/uploads/<filename>', methods=['GET', 'POST'])
+def displayImage(filename):
+    return send_from_directory(directory=app.config['IMAGES_PATH'], filename=filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
